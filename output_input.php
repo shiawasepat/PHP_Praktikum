feff6946e828906a568c59f22034ea8f8f73eb6b
Patrick <shiawasearuji@gmail.com>
2025-09-02 20:17:02 +0800

    out_in + string

--- output_input.php
+++ output_input.php
@@ -1,8 +1,12 @@
 <?php 
+$names = "Pat";
+$products = ["Apple", "Banana", "Orange"];
 // Output & Input
 
     // Output
-        echo "Hello, World!";
+        echo "Hello, World!<br>";
+        echo "My name is $names<br>";
+        echo "My favorite fruits are: " . implode(", ", $products) . "<br>";
     
     // Debug output 
     var_dump($names);
--- string.php
+++ string.php
@@ -3,38 +3,22 @@
 $name = 'Mike';
 // or double quote
 $name = "Mike";
+$email = "mike@example.com";
+$text = "  Hello World  ";
 
 // Double quote string can escape characters \n = new line  \t = tab  \\ = backslash
-echo "Hello Mike\nHello David";
+echo "Hello Mike\nHello David<br>";
 
 // Double quote string can do interpolation
-echo "Hello $name";
-
-// string concat
-echo 'Hello ' . $name;
+echo "Hello $name <br>";
 
 // string length
 echo strlen($name);
 
 // Remove space(s) before and after
 echo trim($text);
-
 // Convert to lowercase / uppercase
 echo strtolower($email);
 echo strtoupper($name);
 
-// Converts the first character to uppercase
-echo ucfirst($name);  // 'Mike' 
-
-// Replace text a by text b in $text
-echo str_replace('a', 'b', $text);
-
-// String Contains (PHP 8)
-echo str_contains($name, 'ke');  // true
-
-// Find numeric position of first occurrence
-$pos = strpos($name, 'k'); // 2
-
-// Returns portion of string (offset / length)
-echo substr($name, 0, $pos);
 ?>
